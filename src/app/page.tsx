060f13027d6f188d8f04af4509a23e5d4d8563f2
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2026-02-24 21:04:33 +0000

    remova esse footer (_for element footer_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Footer } from "@/components/landing/footer";
 import { Hero } from "@/components/landing/hero";
 
 export default function Home() {
   return (
     <main className="bg-background">
       <Hero />
-      <Footer />
     </main>
   );
 }
