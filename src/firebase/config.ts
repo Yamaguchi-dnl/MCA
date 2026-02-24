48109ad1858854fc42d7379e962d676f0a609b47
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2026-02-24 21:29:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9671392272-220ec",
+  "appId": "1:423974307354:web:a2130f13324eff8a9673a3",
+  "apiKey": "AIzaSyAJOC3VXDK9wzH89MUeplgHZYh0QQihT_Y",
+  "authDomain": "studio-9671392272-220ec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "423974307354"
+};
