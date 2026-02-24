e4005bb31f96950ae999687ac7f246299b0c3ad5
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2026-02-24 20:28:27 +0000

    ta cortando o sol na parte de cima (_for element img_)

--- src/app/inscricao/page.tsx
+++ src/app/inscricao/page.tsx
@@ -2,7 +2,7 @@ import { Registration } from "@/components/landing/registration";
 
 export default function InscricaoPage() {
   return (
-    <div className="flex flex-col min-h-[100dvh] items-center bg-background py-8">
+    <div className="flex flex-col min-h-[100dvh] items-center bg-background py-2">
       <Registration />
     </div>
   );
