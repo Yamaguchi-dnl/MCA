1ec25cc698d6310074d6d0633444c2345c5db325
Daniel Yamaguchi <danielyamaguchi409@gmail.com>
2026-02-25 16:41:10 +0000

    todo o seo tem que fazer que é da igreja adventista da promessa

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export function Logo(props: { className?: string }) {
   return (
     <Image
       src="https://ik.imagekit.io/leosmc2zb/Page%20-%20S%C3%A1bado%20total%20MCA/ChatGPT%20Image%2024%20de%20fev.%20de%202026,%2016_56_27.png"
-      alt="Sábado Total MCA Logo"
+      alt="Logo do Sábado Total da Igreja Adventista da Promessa em Barreirinha"
       width={1024}
       height={1024}
       className={props.className}
